4066dfef6d26cfb93fc4705f64c6e1a46251dfd1
Prathamesh Sonpatki <csonpatki@gmail.com>
2012-12-29 18:59:54 +0530

    Fixed specs

--- spec/lmgtfy/runner_spec.rb
+++ spec/lmgtfy/runner_spec.rb
@@ -3,14 +3,18 @@ require 'lmgtfy/runner'
 
 describe Lmgtfy::Runner do
 
+  before do
+    @query = URI.encode_www_form([['q', ARGV.join(' ')]])
+  end
+
   it "accepts search query from the user" do
     runner = Lmgtfy::Runner.new
-    runner.query.should eq ARGV[0]
+    runner.query.should eq @query
   end
 
   it "generates correct url" do
     runner = Lmgtfy::Runner.new
-    runner.generate_lmgtfy_url.should eq "http://lmgtfy.com/?q=#{ARGV[0]}"
+    runner.generate_lmgtfy_url.should eq "http://lmgtfy.com/?#{@query}"
   end
 
 end
