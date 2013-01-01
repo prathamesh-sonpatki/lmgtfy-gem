39dc0719fe13153d234ee0fa421db86f02fcbea5
AbdulKarim Memon <abdulkarimmemon@gmail.com>
2013-01-02 01:49:25 +0530

    Added method to shorten url.

--- lib/lmgtfy/runner.rb
+++ lib/lmgtfy/runner.rb
@@ -1,9 +1,9 @@
+require 'json'
+require 'net/http'
 require 'uri'
 
 module Lmgtfy
-
   class Runner
-
     attr_reader :query
 
     def initialize
@@ -12,6 +12,13 @@ module Lmgtfy
 
     def generate_lmgtfy_url
       "http://lmgtfy.com/?#{@query}"
+    end
+
+    def generate_short_url
+      base_url = "http://is.gd/create.php?format=json&url="
+      string = "http://lmgtfy.com/?#{@query}"
+      response = Net::HTTP.get_response(URI.parse(base_url + string))
+      JSON.parse(response.body)["shorturl"]
     end
 
   end
